497ffb6d20efc4dd564c354f2cdca628bd1ccc90
Vaca Erick <erickyaelvacagorostieta@gmail.com>
2023-10-12 18:27:58 -0600

    modified:   Program/Java/HelloWorld/src/HelloWorld.java

--- Program/Java/HelloWorld/src/HelloWorld.java
+++ Program/Java/HelloWorld/src/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HolaMundo {
+public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola mundo");
+        System.out.println("Hello World");
     }
 }
