1b95eb4f37369b9ff539eb9bbfd126b5925d8caf
Vaca Erick <erickyaelvacagorostieta@gmail.com>
2023-10-14 15:18:04 -0600

    deleted:    Program/C++/ProgramAts/Cpp1_HelloWorld.exe 	renamed:    Program/C++/ProgramAts/Cpp1_HelloWorld.cpp -> Program/C++/ProgramAts/LearningCpp/Example1_HelloWorld/HelloWorld.cpp 	new file:   Program/C++/ProgramAts/LearningCpp/Example1_HelloWorld/HelloWorld.exe 	renamed:    Program/C++/ProgramAts/Cpp1_HelloWorld.png -> Program/C++/ProgramAts/LearningCpp/Example1_HelloWorld/HelloWorld.png 	new file:   Program/Java/Program1001Tips/.vscode/settings.json 	deleted:    Program/Java/ProgramATS/src/Java1_HelloWorld.png 	renamed:    Program/Java/ProgramATS/src/Java1_HelloWorld.java -> Program/Java/ProgramATS/src/LearningJava/Example1_HelloWorld/HelloWorld.java 	new file:   Program/Java/ProgramATS/src/LearningJava/Example1_HelloWorld/HelloWorld.png

--- Program/Java/ProgramATS/src/LearningJava/Example1_HelloWorld/HelloWorld.java
+++ Program/Java/ProgramATS/src/LearningJava/Example1_HelloWorld/HelloWorld.java
@@ -1,4 +1,6 @@
-public class Java1_HelloWorld {
+package LearningJava.Example1_HelloWorld;
+
+public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
     }
